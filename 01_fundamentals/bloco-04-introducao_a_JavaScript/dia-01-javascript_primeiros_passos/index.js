dbcfcb77b83d5564ea32cb809b81c3c2241e727c
felipehfernandes <felipehfernandes1807@gmail.com>
2022-05-12 19:41:55 -0300

    exercicios 4.1

---
+++ 01_fundamentals/bloco-04-introducao_a_JavaScript/dia-01-javascript_primeiros_passos/index.js
@@ -0,0 +1,56 @@
+const a = 0;
+const b = 60;
+const c = 30;
+
+// ********** 1
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// ********** 2
+// if (a > b) {
+//     console.log(a)
+// }
+
+// else {
+//     console.log(b)
+// }
+
+// ********** 3
+// if (a > b && a > c) {
+//     console.log(a)
+// }
+
+// else if (b > a && b > c) {
+//     console.log(b)
+// }
+
+// else if (c > a && c > b) {
+//     console.log(c)
+// }
+
+// ********** 4
+// if (a > 0) {
+//     console.log("positive");
+// }
+
+// else if (a < 0){
+//     console.log("negative")
+// }
+
+// else {
+//     console.log("zero")
+// }
+
+// ********** 5
+// if (a + b + c == 180 && a>=0 && b>=0 && c>=0) {
+//     console.log(true);
+// }
+
+// else {
+//     console.log(false);
+// }
+
+// ********** 6
